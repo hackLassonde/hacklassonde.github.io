a0b4b24f42eed571c05a43d86cf402de2f7186c3
Connor Dear <connordear@gmail.com>
2019-02-23 17:10:42 -0700

    added star fadein/fadeout

--- js/star-script.js
+++ js/star-script.js
@@ -4,4 +4,4 @@ const stars2 = document.getElementById("stars-2");
 setInterval(() => {
   stars1.classList.toggle("star-bg-1-show");
   stars2.classList.toggle("star-bg-2-show");
-}, 2000);
+}, 3000);
